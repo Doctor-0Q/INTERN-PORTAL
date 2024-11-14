a3a606507f55cdd49f4a225264d5ea4e283f8dc7
Aman Singh <amansingh10122003@gmail.com>
2024-11-15 01:26:28 +0530

    Removed auto scroll when navigation from one page to another happens.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./MainLayout";
 import AdminLayout from "./AdminLayout";
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           {/* User routes */}
           <Route path="/*" element={<MainLayout />} />
