752b286f6be107ca759fca5064c914d6cf9371ec
priyanshu442004 <priyanshusharma442004@gmail.com>
2024-11-17 17:02:26 +0530

    Ticket

--- client/src/components/TicketDetails.jsx
+++ client/src/components/TicketDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
 import SlideToReply from './SlideToReply';
 import toast from 'react-hot-toast';
