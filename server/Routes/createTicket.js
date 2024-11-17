5559e8e2f393b401d843ef6ecda9a9e7d45fa2be
priyanshu442004 <priyanshusharma442004@gmail.com>
2024-11-17 15:51:48 +0530

    Ticket

--- server/Routes/createTicket.js
+++ server/Routes/createTicket.js
@@ -1,10 +1,12 @@
 import { createSupportTicket, setTicketResponse } from "../Controllers/saveSupportTicket.js";
 import express from 'express'
-import { getAllSupportTickets } from "../Controllers/supportTicketController.js";
+import { closeTicket, deleteTicket, getAllSupportTickets } from "../Controllers/supportTicketController.js";
 
 const router =express.Router();
 
 router.route('/createTicket').post(createSupportTicket);
 router.route('/tickets').get(getAllSupportTickets);
 router.route('/ticketResolve/:ticketID').post(setTicketResponse);
+router.route('/ticketDelete/:ticketID').post(deleteTicket);
+router.route('/ticketClosing/:ticketID').post(closeTicket);
 export default router;
