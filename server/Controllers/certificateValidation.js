3f820d7ad77c7dafbc133c2536c9bf3c12748faf
priyanshu442004 <145048420+priyanshu442004@users.noreply.github.com>
2024-12-09 23:54:59 +0530

    Update certificateValidation.js

--- server/Controllers/certificateValidation.js
+++ server/Controllers/certificateValidation.js
@@ -1,15 +1,16 @@
-import {Certificate} from '../Model/certificateModel.js'
+import {Intern} from '../Model/Intern.js'
 
 export const certificateValidation=async(req,res)=>{
     try {
         const {id}=req.body;
-        const isValid=await Certificate.findOne({certificateId:id})
+        const isValid=await Intern.findOne({certificateId:id})
         if(!isValid){
             return res.status(400).json({message:"This certificate is invalid",success:false})
         }
         return res.status(200).json({message:"Certificate is valid",success:true})
     } catch (error) {
-        console.log(error)
+        console.log(erro)
         res.status(400).json({message:"Internal error",success:false})
     }
 }
+
